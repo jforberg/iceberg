c840536e13c059a14e2e969fc5fd02d46ac5d88e
Johan Förberg <johan@forberg.se>
2014-07-26 19:36:46 +0200

    Better test coverage of iceberg.glacier.

--- test/iceberg/t_glacier.clj
+++ test/iceberg/t_glacier.clj
@@ -2,5 +2,31 @@
   (:use midje.sweet)
   (:require [iceberg.glacier :as glacier]))
 
+(def account {:region :tokyo
+              :number "123"
+              :keyid "AKIA123"
+              :apikey "ABC123"})
+
+(facts "about `list-vaults-req`")
+
+(facts "about `deserialize-body`"
+  (fact "handles the empty (string) case"
+    (glacier/deserialize-body {:body "{}"})
+      => {:body {}})
+
+  (fact "handles the empty (map) case"
+    (glacier/deserialize-body {:body {}}) => {:body {}}))
+
 (facts "about `encode-body`"
-  (fact "Handles the empty case"))
+  (fact "handles request with no body"
+    (glacier/encode-body {:test 123}) => {:test 123})
+
+  (fact "handles empty body"
+    (let [req (glacier/encode-body {:body ""})]
+      (vec (:body req)) => []
+      (:headers req) => {:content-length "0"}))
+  
+  (fact "handles non-empty body"
+    (let [req (glacier/encode-body {:body "message"})]
+      (vec (:body req)) => (map byte "message")
+      (:headers req) => {:content-length "7"})))
